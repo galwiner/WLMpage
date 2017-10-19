b62e037931f00f8080eb2691fbc192e692c48a34
galwiner <gal.winer@weizmann.ac.il>
2017-10-19 15:20:14 +0300

    bidirectional communication set up

--- WLMpage.py
+++ WLMpage.py
@@ -7,14 +7,16 @@ import random
 
 
 import os
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, json
 
 import utilities.database_utilities as db
 import threading
 import atexit
 import datetime
-import json
-POOL_TIME = 0.1 #Seconds
+
+# import json
+
+POOL_TIME = 0.1  # Seconds
 
 # variables that are accessible from anywhere
 commonDataStruct = []
@@ -22,7 +24,6 @@ commonDataStruct = []
 dataLock = threading.Lock()
 # thread handler
 yourThread = threading.Thread()
-
 
 
 def create_app():
@@ -41,19 +42,16 @@ def create_app():
         yourThread = threading.Timer(POOL_TIME, doBackgroundTask, ())
         yourThread.start()
 
-
-
     def doBackgroundTask():
         global commonDataStruct
         global yourThread
         with dataLock:
-        # Do your stuff with commonDataStruct Here
+            # Do your stuff with commonDataStruct Here
             commonDataStruct.append(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
         # Set the next thread to happen
         yourThread = threading.Timer(POOL_TIME, doBackgroundTask, ())
         yourThread.start()
-
 
     startup()
     # When you kill Flask (SIGTERM), clear the trigger for the next thread
@@ -61,47 +59,45 @@ def create_app():
     return app
 
 
+app = create_app()
 
 
-
-
-
-
-
-
-app=create_app()
 @app.route('/_pollWLM')
 def readWLM():
     return jsonify(result=random.randint(0, 1000))
 
 
-@app.route('/_getWLMData')
+@app.route('/_getWLMData/')
 def getWLMData():
     with dataLock:
+
         if len(commonDataStruct) > 1:
-            return jsonify(result=commonDataStruct.pop()+ str(len(commonDataStruct)))
+            # activeChannels=[i for i, e in enumerate(a) if e != 0]
+            dat={i: random.randint(0, 10) for i in range(1, 9)}
+            return jsonify(dat)  # returning data from the server to the client works
+            # return jsonify(result=my_var)
         else:
             return jsonify(result='Empty')
 
 
 @app.route('/_setWLMChannels', methods=['POST'])
 def setWLMChannels():
-
-    print(request.args)
-    return json.dumps({'status': 'OK'});
+    commonDataStruct = request.json['activeChannels']
+    return json.dumps({'status': 'OK'})
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 if __name__ == '__main__':
     # app.run(host='10.10.10.3', port='5000')
     app.run(debug=True)
 # working code to get frequency for WLM
 
-#r.lib.Operation(r.cCtrlStartMeasurement)
-#r.lib.SetSwitcherMode(1)
-#get_wavelength = r.lib.GetFrequencyNum
-#get_wavelength.restype = c_double
-#freq = get_wavelength(1,0)
+# r.lib.Operation(r.cCtrlStartMeasurement)
+# r.lib.SetSwitcherMode(1)
+# get_wavelength = r.lib.GetFrequencyNum
+# get_wavelength.restype = c_double
+# freq = get_wavelength(1,0)
